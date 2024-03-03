b749c0ae5b3cdec28249ae01bcbc8153063155f5
Kristjan Lääne <xonproject@gmail.com>
2024-03-03 15:56:35 +0200

    Search feed start

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -33,7 +33,11 @@ const ChannelDetail = () => {
             zIndex: 10,
           }}
         />
-        <ChannelCard channelDetail={channelDetail} marginTop='-93px' />
+        <ChannelCard channelDetail={channelDetail} marginTop='-110px' />
+      </Box>
+      <Box display='flex' p={2}>
+        <Box sx={{ mr: { sm: '100px' } }} />
+        <Videos videos={videos} />
       </Box>
     </Box>
   );
